efb0b804327d37b1be4db39178fb47ef5734aa8e
vickynocampoemail <you@example.com>
2022-10-06 12:41:33 -0300

    primer resultado de listar libros en componente search

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,39 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function SearchBooks(){
+	const [books, setBooks]  = useState([]);
 
-	const [books, setbooks]  = useState([]);
-	const [keyword, setKeyword] = useState("ACTION");
-	const [search, setSearch] = useState("")
-
-	useEffect( () => {
-		console.log("se monto el componente")
-		fetch(`http://localhost:3030/api/products/`)
-		// http://localhost:3030/api/products/
+useEffect(() => {
+	 console.log("se monto el componente")
+	 fetch(`http://localhost:3030/api/products/`)
 		.then(response => response.json())
 		.then(data => {
 			console.log("data")
 			console.log(data)
-			setbooks(data.Search)
-		})
+			setBooks(data.products)})
 		.catch(error => console.error(error));
-	}, [keyword])
+}, [])
 
-	// const handlerSubmit = (e)=> {
-	// 	e.preventDefault();
-	// 	setKeyword(search);
-	// }
-
-	// const handleChange = (e)=>{
-	// 	setSearch(e.target.value)
-	// }
-
-	// // Credenciales de API
-	// const apiKey = 'a9d996d6'; // Intenta poner cualquier cosa antes para probar
-
-	return(
-		<div className="container-fluid">
-		
+return(
+		<div className="container-fluid">		
+			<br/>
+			<h4>Listado de Libros</h4>
+			<hr/>
+			<br/>
+			<ul>
+				{books.length === 0 && <p>Cargando...</p>}
+				{
+					books.map((book, i) => {
+						return (
+							<li key={i}>
+								<h5>{book.name}</h5>
+								<h6>{book.description}</h6>
+								<br/>
+							</li>
+						)			
+					})
+				}
+			</ul>		
 		</div>
 	)
 }
